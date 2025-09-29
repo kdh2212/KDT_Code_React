09e19b18c6f81569fb1c30084a8a75b420e2cc6d
kdh2212 <jjun8248@gmail.com>
2025-09-29 18:19:06 +0900

    React : Example Code 2025/09/29

---
+++ my-react05/src/component/Body.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+export default function Body() {
+
+    useEffect(() => {
+        console.log("컴포넌트가 처음 마운트됨");
+
+        return() => {
+
+            console.log('컴포넌트가 인마운트될 때 실행됨');
+        };
+    },[]); // 빈 배열 -> 한 번만 실행
+
+
+    return(
+        <>
+            <h1>바디</h1>
+        </>
+    );
+   
+}
+
+
+
